1ae39e3f7c8c901760f04c4a86eb85bf154f244d
lorenz <looooo@users.noreply.github.com>
2022-08-19 16:07:01 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,7 @@ cmake -G "Ninja" ^
     -D Python3_FIND_STRATEGY:STRING=LOCATION ^
     -D Python3_FIND_REGISTRY:STRING=NEVER ^
     -D SWIG_HIDE_WARNINGS:BOOL=ON ^
+    -D PYTHONOCC_MESHDS_NUMPY=ON ^
     ..
 
 if errorlevel 1 exit 1
